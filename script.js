3db622dccc466840380e3c61e669822d543dc572
Moringa School <lizdmukami@gmail.com>
2022-11-21 01:55:50 +0300

    Completed Portolio

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function opentab(event, tabname) {
+    var i, tabcontents, tablinks;
+    tabcontent = document.getElementsByClassName("tab-contents");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tab-links");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabname).style.display = "block";
+    event.currentTarget.className += " active";
+  }
