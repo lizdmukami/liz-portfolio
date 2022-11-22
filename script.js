ce203e9b6a42fd0f297d6102e8ba5cc46dbff93f
Moringa School <lizdmukami@gmail.com>
2022-11-22 21:33:31 +0300

    Added hamburger menu

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
 
 function opentab(event, tabname) {
     var i, tabcontents, tablinks;
